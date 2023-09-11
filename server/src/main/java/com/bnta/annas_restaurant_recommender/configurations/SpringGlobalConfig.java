c39665f4fd5775ce850426b71aa4ce4b1089577f
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-09-11 11:40:24 +0100

    add CORS

---
+++ server/src/main/java/com/bnta/annas_restaurant_recommender/configurations/SpringGlobalConfig.java
@@ -0,0 +1,18 @@
+package com.bnta.annas_restaurant_recommender.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringGlobalConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
+
+
