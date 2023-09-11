db955e79b59c39b53afb2c053e2873154fe3afbc
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-09-11 11:40:54 +0100

    added fetch

--- client/src/containers/RestaurantContainer.js
+++ client/src/containers/RestaurantContainer.js
@@ -1,4 +1,29 @@
+import {useState,useEffect} from "react";
+
 const RestaurantContainer = () =>{
+
+    // create the  initial states
+    const [restaurantList, setRestaurantList] = useState([]); 
+    const [filteredList, setFilteredList] = useState([]);
+    const [favouritesList, setFavouritesList] = useState([]);
+
+    // fetch restaurant data from data loader
+    const fetchRestaurants = async () =>{ 
+        const response = await fetch("http://localhost:8080/restaurants"); 
+        const data = await response.json()
+        setRestaurantList(data);
+        setFilteredList(data);
+        console.log(data);
+        
+        
+    }
+
+    // fetch data on load at the start of the run
+    useEffect(() => {
+        fetchRestaurants();
+
+    },[])
+
     return(
         <>
         
