a40333c2118d3739d0622c0f2d895d3654db8a93
nimo-mohamed <nimoxmohamed@gmail.com>
2023-09-14 12:42:25 +0100

    created footer and added css for it

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,35 @@
+import React from 'react';
+
 const Footer = () => {
-
-    return(
-        <>
-            <p className="footer">This is the Footer</p>
-        </>
-    )
-}
-
-
-
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        {/* Contact Us Section */}
+                        <h3>Contact Us</h3>
+                        <p>Email: Anna'sRecommends@example.com</p>
+                        <p>Phone: +1 (123) 456-7890</p>
+                    </div>
+                    <div className="col-md-4">
+                        {/* Follow Us Section */}
+                        <h3>Follow Us</h3>
+                        <ul className="social-icons">
+                            {/* Social Media Links */}
+                            <li><a href="https://en-gb.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a></li>
+                            <li><a href="https://twitter.com/?lang=en-gb" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a></li>
+                            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4">
+                        {/* Copyright Section */}
+                        <h3>&copy; 2023 Your Website</h3>
+                        <p>All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
 export default Footer;
